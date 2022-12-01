7b10c50c2d8705dba57921e6e1b8e0a0c4f1070c
thibault-roux <thibault.roux1@laposte.net>
2022-12-01 14:07:12 +0100

    Update launch for pos confusion matrix

--- launch.py
+++ launch.py
@@ -2,6 +2,7 @@
 import argparse
 import utils.make_datasets as mk
 import utils.eval as eval
+from utils.matrix import pos_matrix
 
 
 if __name__ == '__main__':
@@ -18,7 +19,9 @@ if __name__ == '__main__':
     parser.add_argument("-s", "--semdist", help="SemDist", action="store_true")
     parser.add_argument("-b", "--bertscore", help="BERTScore", action="store_true")
     # parser.add_argument("-l", "--lcer", help="Lemma Character Error Rate", action="store_true") # As it is proven that this metric is useless, we could delete it
+    parser.add_argument("--cmpos", help="Confusion matrix of POS", action="store_true")
     args = parser.parse_args()
+    argsid = args.namef.split("/")[1]
 
     print("Starting datasets' generation...")
     # if args.cer or args.ember or args.semdist:
@@ -31,30 +34,35 @@ if __name__ == '__main__':
         mk.dataset4(args.namef) # useful for LER and LCER
     print("Datasets' generation done.")
 
+    # launch evaluation
+    if args.wer or args.cer or args.ler or args.dposer or args.uposer or args.ember or args.semdist or args.bertscore:
+        print("Starting evaluation...")
+        fresults = open("results/"+argsid+".txt","w", encoding="utf8") # File containing the results.
+        if args.cer:
+            eval.cer(argsid, fresults)
+        if args.ember:
+            import numpy as np
+            from scipy import spatial
+            #eval.ember(argsid, fresults, threshold=0.4)
+            eval.ember(argsid, fresults, threshold=1)
+        if args.semdist:
+            eval.semdist(argsid, fresults)
+        if args.wer:
+            eval.wer(argsid, fresults)
+        if args.uposer or args.dposer:
+            mapper = eval.prepare_POS(argsid) # Checking POS and loading of mapping 
+            if args.uposer:
+                eval.uposer(argsid, fresults, mapper)
+            if args.dposer:
+                eval.dposer(argsid, fresults)
+        if args.ler: # or args.lcer:
+            eval.ler(argsid, fresults)
+        if args.bertscore:
+            eval.bertscore(argsid, fresults)
+        fresults.close()
+        print("Evaluation completed!")
 
-    print("Starting evaluation...")
-    argsid = args.namef.split("/")[1]
-    fresults = open("results/"+argsid+".txt","w", encoding="utf8") # File containing the results.
-    if args.cer:
-        eval.cer(argsid, fresults)
-    if args.ember:
-        import numpy as np
-        from scipy import spatial
-        #eval.ember(argsid, fresults, threshold=0.4)
-        eval.ember(argsid, fresults, threshold=1)
-    if args.semdist:
-        eval.semdist(argsid, fresults)
-    if args.wer:
-        eval.wer(argsid, fresults)
-    if args.uposer or args.dposer:
-        mapper = eval.prepare_POS(argsid) # Checking POS and loading of mapping 
-        if args.uposer:
-            eval.uposer(argsid, fresults, mapper)
-        if args.dposer:
-            eval.dposer(argsid, fresults)
-    if args.ler: # or args.lcer:
-        eval.ler(argsid, fresults)
-    if args.bertscore:
-        eval.bertscore(argsid, fresults)
-    fresults.close()
-    print("Evaluation completed!")
+    # Compute confusion matrix
+    if args.cmpos:
+        print("Compute Part-of-speech Confusion Matrix.")
+        pos_matrix(argsid, ignore_correct=False, ignore_pos=["X", "PART", "SYM", "PUNCT", "<eps>"])
