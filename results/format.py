bbbcbafe8392e73ec91fa90e2817a748b95121c8
thibault-roux <thibault.roux1@laposte.net>
2022-12-07 11:36:57 +0100

    Better format for results

---
+++ results/format.py
@@ -0,0 +1,33 @@
+
+
+
+with open("systems.txt", "r", encoding="utf8") as file:
+    systems = []
+    for ligne in file:
+        systems.append(ligne.split(".txt")[0])
+
+metrics = ['WER', 'CER', 'uPOSER', 'dPOSER', 'LER', 'EmbER', 'SemDist', 'BERTScore']
+
+scores = dict()
+for system in systems:
+    scores[system] = dict()
+    with open(system + ".txt", "r", encoding="utf8") as file:
+        for ligne in file:
+            line = ligne[:-1].split(": ")
+            metric = line[0]
+            score = int(1000*float(line[1]))/1000
+            scores[system][metric] = score
+
+
+txt = ","
+for metric in metrics:
+    txt += metric + ","
+txt = txt[:-1] + "\n"
+for system, scores_sys in scores.items():
+    txt += system + ","
+    for metric in metrics:
+        txt += str(scores_sys[metric]) + ","
+    txt = txt[:-1] + "\n"
+
+with open("results.txt", "w", encoding="utf8") as file:
+    file.write(txt)
