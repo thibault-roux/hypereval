1dbb64c54b152a1497cf63a3348eca96bc4bb0ce
thibault-roux <thibault.roux1@laposte.net>
2022-12-21 11:41:43 +0100

    Finalised version

--- data/splitter.py
+++ data/splitter.py
@@ -1,9 +1,9 @@
+import os
 
 
-systems = ["KD_woR", "KD_wR", "SB_bpe1000", "SB_bpe750", "SB_s2s", "SB_w2v_1k", "SB_w2v_3k", "SB_w2v_7k", "SB_xlsr_fr", "SB_xlsr"]
+systems = ["SB_w2v", "KD_woR", "KD_wR", "SB_bpe1000", "SB_bpe750", "SB_s2s", "SB_w2v_1k", "SB_w2v_3k", "SB_w2v_7k", "SB_xlsr_fr", "SB_xlsr"]
+# systems = ["KD_woR"]
 
-
-# dico[ref][hyp"] = [KD_wR, SB_w2v, SB_xlsr]
 
 def add(dico, ref, hyp, system):
     if ref in dico:
@@ -13,6 +13,7 @@ def add(dico, ref, hyp, system):
             dico[ref][hyp] = [system]
     else:
         dico[ref] = dict()
+        dico[ref][hyp] = [system]
 
 
 def removeEPS(sentence):
@@ -24,8 +25,8 @@ def removeEPS(sentence):
     return ' '.join(new_sentence_list)
 
 
-dico = dict()
-for system in systems:
+dico = dict() # dico[ref][hyp] = [KD_wR, SB_w2v, SB_xlsr]
+for system in systems: # Associer reference, hypothesis to their system
     with open(system+"/"+system+"1.txt", "r", encoding="utf8") as file:
         for ligne in file:
             line = ligne.lower().split("\t")
@@ -33,20 +34,61 @@ for system in systems:
             hyp = removeEPS(line[2])
             add(dico, ref, hyp, system)
 
+
 subsets = dict()
 for system in systems:
-    subsets[system] = ""
+    subsets[system] = set()
 
+# Récupérer les transcriptions de autoselect dans subsets
+hyp1error = 0
+hyp2error = 0
+referror = 0
+correct = 0
 with open("autoselect.txt", "r", encoding="utf8") as file:
     for ligne in file:
         line = ligne.split("\t")
+        id = line[0]
         ref = line[1]
         hyp1 = line[2]
         hyp2 = line[3]
         
         if ref in dico:
+            # print(ref)
+            # print(hyp1)
+            # input()
             if hyp1 in dico[ref]:
-                systs = dico[ref][hyp]
+                correct += 1
+                systs = dico[ref][hyp1]
                 for sys in systs:
+                    subsets[sys].add(id + "\t" + ref + "\t" + hyp1 + "\t_\n")
+            else:
+                hyp1error += 1
+            if hyp2 in dico[ref]:
+                correct += 1
+                systs = dico[ref][hyp2]
+                for sys in systs:
+                    subsets[sys].add(id + "\t" + ref + "\t" + hyp2 + "\t_\n")
+            else:
+                hyp2error += 1
+        else:
+            referror += 1
 
-# mettre autoselect dans hypereval et lancer l'expé
+# Write subsets in files
+
+for system in systems:
+    txt = ""
+    for t in subsets[system]:
+        txt += t
+
+    path = os.path.join(os.getcwd(), "subset_" + system)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    print(path)
+    with open(path+"/refhyp.txt", "w", encoding="utf8") as file:
+        file.write(txt)
+    
+print("Terminus.")
+print("hyp1error:", hyp1error)
+print("hyp2error:", hyp2error)
+print("referror:", referror)
+print("correct:", correct)
